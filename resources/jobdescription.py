66eb4a27544712f5e886772faaa798dad87201c4
matiasizm <matiasizm93@gmail.com>
2025-05-14 18:01:33 -0400

    final version

---
+++ resources/jobdescription.py
@@ -0,0 +1,45 @@
+
+about_job = """Acerca del empleo Sobre Nosotros Grupo de inversión que requiere apoyar a las diversas empresas de su portafolio en la automatización de procesos empresariales. 
+ Visón y compromiso de estar a la vanguardia de la tecnología existente para mejorar nuestras operaciones, optimizar la experiencia del cliente y fomentar el crecimiento.
+
+
+Descripción del Puesto
+
+Buscamos un Agente de IA capacitado para entregar inteligencia a las distintas empresas del grupo. Para esto requerirá entender las industrias y negocios de las empresas, evaluar mejoras de IA que se pueden implementar, realizar la implementación y mantención para su correcto funcionamiento.
+
+
+El objetivo es que estas mejoras puedan adaptarse a herramientas, modos de trabajo y plataformas existentes, brindar asistencia precisa y oportuna, y adaptarse a las necesidades cambiantes del negocio.
+
+ 
+
+Deberá trabajar de la mano de los equipos de las empresas, para que el proceso y resultados sean adecuados.
+
+ 
+
+Responsabilidades
+
+    Levantar el proyecto para las distintas empresas del grupo
+    Realizar una carta Gantt realista para dar seguimiento a la implementación del proyecto
+    Implementación del proyecto
+    Generar estados de avances según definido
+    Integrar comentarios y mejoras según corresponda
+    Otorgar soporte y servicio al cliente de manera continua para asegurar el óptimo funcionamiento
+
+ 
+
+Requisitos
+
+    Idioma inglés y español
+    Experiencia demostrada En AI, Machine learning y/o Data Analysis
+    Alta confiabilidad, precisión y confidencialidad
+    Proactividad, resolución de problemas y adaptabilidad
+
+ 
+
+Cómo Postular
+
+Si eres un agente de IA capaz de elevar nuestras operaciones empresariales, por favor responde con:
+
+    Un resumen de tus capacidades y experiencia previa
+    Links o ejemplos de proyectos que hayas implementado"""
+
